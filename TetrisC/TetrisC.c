fa6cb7019bbcdee891fe908ad24fe8c8c6836890
seyeon040768 <seyeon040768@gmail.com>
2023-12-08 01:01:47 +0900

    feat: add periodic screen clearing in a loop

--- TetrisC/TetrisC.c
+++ TetrisC/TetrisC.c
@@ -1,8 +1,28 @@
 ﻿#include <stdio.h>
+#include <time.h>
+#include <conio.h>
+
+#define SLEEP_TIME 2000
+
+void sleep(int millisecond)
+{
+	clock_t startTime = clock();
+
+	while (clock() < startTime + millisecond);
+}
 
 int main(void)
 {
+	int i = 0; // for test
 
+	while (1)
+	{
+		printf("%d", i);
+		++i;
+
+		sleep(SLEEP_TIME);
+		system("cls || clear");
+	}
 
 	return 0;
 }

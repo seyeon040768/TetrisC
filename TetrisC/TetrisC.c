d2e5d75ddab5b04c26bad3bd9a5f3b9fda2bc9f6
seyeon040768 <seyeon040768@gmail.com>
2023-12-08 20:25:56 +0900

    feat: change gamearea do not include edge

--- TetrisC/TetrisC.c
+++ TetrisC/TetrisC.c
@@ -17,51 +17,52 @@ void sleep(int millisecond)
 
 void ClearGameArea(char* gameArea, int height, int width)
 {
-	int h = 0, w = 0;
-
-	for (w = 0; w < width; ++w)
+	for (int h = 0; h < height; ++h)
 	{
-		*(gameArea + w) = GAME_AREA_EDGE;
-		*(gameArea + (height - 1) * width + w) = GAME_AREA_EDGE;
-	}
-
-	for (h = 1; h < height - 1; ++h)
-	{
-		*(gameArea + h * width) = GAME_AREA_EDGE;
-		for (w = 1; w < width - 1; ++w)
+		for (int w = 0; w < width; ++w)
 		{
 			*(gameArea + h * width + w) = ' ';
 		}
-		*(gameArea + h * width + w) = GAME_AREA_EDGE;
 	}
 }
 
 void PrintGameArea(char* gameArea, int height, int width)
 {
+	for (int i = 0; i < width + 2; ++i)
+	{
+		printf("%c", GAME_AREA_EDGE);
+	}
+	printf("\n");
+
 	for (int h = 0; h < height; ++h)
 	{
+		printf("%c", GAME_AREA_EDGE);
 		for (int w = 0; w < width; ++w)
 		{
 			printf("%c", *(gameArea + h * width + w));
 		}
-		printf("\n");
+		printf("%c\n", GAME_AREA_EDGE);
 	}
+
+	for (int i = 0; i < width + 2; ++i)
+	{
+		printf("%c", GAME_AREA_EDGE);
+	}
+	printf("\n");
 }
 
 int main(void)
 {
-	char gameArea[GAME_AREA_HEIGHT + 2][GAME_AREA_WIDTH + 2];
+	char gameArea[GAME_AREA_HEIGHT][GAME_AREA_WIDTH];
 
 	while (1)
 	{
-		ClearGameArea(&gameArea, GAME_AREA_HEIGHT + 2, GAME_AREA_WIDTH + 2);
-
-
+		ClearGameArea(gameArea, GAME_AREA_HEIGHT, GAME_AREA_WIDTH);
 
 		sleep(SLEEP_TIME);
 		system("cls || clear");
 		
-		PrintGameArea(&gameArea, GAME_AREA_HEIGHT + 2, GAME_AREA_WIDTH + 2);
+		PrintGameArea(gameArea, GAME_AREA_HEIGHT, GAME_AREA_WIDTH);
 	}
 
 	return 0;
